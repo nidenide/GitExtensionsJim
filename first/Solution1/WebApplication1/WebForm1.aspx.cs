ca8be609353442557dd7dbd601357a17d84cf294
nidenide <39553612@qq.com>
2018-09-30 11:34:34 +0800

    1134

--- first/Solution1/WebApplication1/WebForm1.aspx.cs
+++ first/Solution1/WebApplication1/WebForm1.aspx.cs
@@ -7,11 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace WebApplication1
 {
-    public partial class _Default : System.Web.UI.Page
+    public partial class WebForm1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            int i = 111111111;
         }
     }
 }
